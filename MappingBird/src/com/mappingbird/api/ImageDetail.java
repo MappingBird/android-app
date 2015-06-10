a5d446939d9ba85e6ebdc4f3ac0a763944775e6e
Hao Liang Shiau <genesttw@gmail.com>
2015-06-10 23:56:56 +0800

    UI : Image Path
    先判斷Mapping bird的 image path改成https

--- MappingBird/src/com/mappingbird/api/ImageDetail.java
+++ MappingBird/src/com/mappingbird/api/ImageDetail.java
@@ -12,10 +12,17 @@ public class ImageDetail implements Serializable{
 	private String mIUpdateTime;
 	private long mIPointId;
 
+	// TODO : 以後要修正
+	private static final String CHANGE_HEADER = "http://mappingbird.com/media";
+	private static final String NEW_HEADER = "https://mappingbird.com/media";
 	ImageDetail(long id, String thumbpath, String url, String createTime, String updateTime, long pointId ) {
 		mIId = id;
 		mThumbPath = thumbpath;
-		mIUrl = url;
+		if(url.contains(CHANGE_HEADER)) {
+			mIUrl = NEW_HEADER + url.substring(CHANGE_HEADER.length());
+		} else {
+			mIUrl = url;
+		}
 		mICreateTime = createTime;
 		mIUpdateTime = updateTime;
 		mIPointId = pointId;
@@ -23,7 +30,11 @@ public class ImageDetail implements Serializable{
 	public ImageDetail(long id, String thumbpath, String url) {
 		mIId = id;
 		mThumbPath = thumbpath;
-		mIUrl = url;
+		if(url.contains(CHANGE_HEADER)) {
+			mIUrl = NEW_HEADER + url.substring(CHANGE_HEADER.length());
+		} else {
+			mIUrl = url;
+		}
 	}
 	public long getId() {
 		return mIId;
