b571797e3dcbf3a3279697b2038437050efd85f5
Hao Liang Shiau <genesttw@gmail.com>
2015-08-14 00:09:33 +0800

    [Tag] Tag String layout
    修正 Tag的背景顏色, 高度和寬度

--- MappingBird/src/com/mappingbird/collection/widget/MBSpannBackground.java
+++ MappingBird/src/com/mappingbird/collection/widget/MBSpannBackground.java
@@ -3,9 +3,12 @@ package com.mappingbird.collection.widget;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.graphics.Paint.FontMetricsInt;
+import android.graphics.Rect;
 import android.graphics.RectF;
+import android.os.Debug;
 import android.text.style.ReplacementSpan;
 
+import com.mappingbird.common.DeBug;
 import com.mappingbird.common.MappingBirdApplication;
 import com.mpbd.mappingbird.R;
 
@@ -18,35 +21,51 @@ public class MBSpannBackground extends ReplacementSpan{
 	
 	private int mPaddingTop = 0;
 	private int mPaddingBottom = 0;
+	
+	private int mPaddingSpace = 0;
+
 	public MBSpannBackground(int color) {
 	    this.mBackgroundColor = color;
 	    mPaddingLeft = (int)MappingBirdApplication.instance().getResources().getDimension(
-	    		R.dimen.coll_list_item_tag_span_padding_left);
+	    		R.dimen.tag_span_padding_left);
 	    mPaddingRight = (int)MappingBirdApplication.instance().getResources().getDimension(
-	    		R.dimen.coll_list_item_tag_span_padding_right);
+	    		R.dimen.tag_span_padding_right);
 	    mPaddingTop = (int)MappingBirdApplication.instance().getResources().getDimension(
-	    		R.dimen.coll_list_item_tag_span_padding_top);
+	    		R.dimen.tag_span_padding_top);
 	    mPaddingBottom = (int)MappingBirdApplication.instance().getResources().getDimension(
-	    		R.dimen.coll_list_item_tag_span_padding_botton);
+	    		R.dimen.tag_span_padding_bottom);
+	    
+	    mPaddingSpace = (int)MappingBirdApplication.instance().getResources().getDimension(
+	    		R.dimen.tag_span_space);
 	}
 
 	@Override
 	public void draw(Canvas canvas, CharSequence text, int start, int end,
 			float x, int top, int y, int bottom, Paint paint) {
+
+		CharSequence subStr = text.subSequence(start, end);
+		Rect bounds = new Rect();
+		paint.getTextBounds(subStr.toString(), 0, subStr.length(), bounds);
+		int strHeight = bounds.bottom - bounds.top;
+		int strTop = bounds.top;
+
+		int textY = (bottom - top - strHeight)/2 - strTop;
 	    RectF rect = new RectF(x, top-mPaddingTop, x + measureText(paint, text, start, end), bottom+mPaddingBottom);
 	    Paint paintbg = new Paint();
 	    paintbg.setColor(mBackgroundColor);
 	    canvas.drawRect(rect, paintbg);
 	    canvas.save();
 	    canvas.translate(mPaddingLeft, 0);
-	    canvas.drawText(text, start, end, x, y, paint);
+	    canvas.drawText(text, start, end, x, textY, paint);
 	    canvas.restore();
 	}
 
 	@Override
 	public int getSize(Paint paint, CharSequence text, int start, int end,
 			FontMetricsInt fm) {
-		return Math.round(measureText(paint, text, start, end));
+		Rect bounds = new Rect();
+		paint.getTextBounds(text.toString(), 0, text.length(), bounds);
+		return Math.round(measureText(paint, text, start, end))+mPaddingSpace;
 	}
 
 	private float measureText(Paint paint, CharSequence text, int start, int end) {
